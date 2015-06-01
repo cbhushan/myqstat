e5248ff57db828bf154b7418a32025a285ee3a74
C Bhushan <cbhushan@users.noreply.github.com>
2015-05-31 21:48:45 -0700

    closer to working

--- myqstat.py
+++ myqstat.py
@@ -1,29 +1,33 @@
-#!/usr/bin/env
+#!/usr/bin/python
 import os
 import xml.etree.ElementTree as ET
 import subprocess
 
-def printSingleJobsDetails(t):
-    f = os.popen('qstat -x')
-    tree = ET.parse(f)
-    root = tree.getroot()
-    print "Job_Id   walltime state     nodes       Job_Name"
-    print "------   -------- ----- --------------- --------------------------"
+def printSingleJobsDetails(id):
+    p = subprocess.Popen(['qstat', '-f', '-x', id], stdout=subprocess.PIPE)
+    output = p.communicate()[0].splitlines()
+    root = ET.fromstring(output[0])
+
     for job in root:
-        print job.find('Job_Id').text, " ",
-        print job.find('resources_used').find('walltime').text, " ",
+        print id + " ",
         print job.find('job_state').text, " ",
         print job.find('Resource_List').find('nodes').text, " ",
         print job.find('Job_Name').text
 
 
 def myqstat(t):
-#    p = os.popen('qstat -x')
+    user = os.environ['USER']
 
-    p = subprocess.Popen(['ls', '-1'], stdout=subprocess.PIPE)
-    lines = p.communicate()[0].splitlines()
+    proc1 = subprocess.Popen(['qstat', '-u', user], stdout=subprocess.PIPE)
+    proc2 = subprocess.Popen(['grep', user],stdin=proc1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc1.stdout.close() # Allow proc1 to receive a SIGPIPE if proc2 exits.
+
+    lines = proc2.communicate()[0].splitlines()
+
+    print "Job_Id   walltime state     nodes       Job_Name"
+    print "------   -------- ----- --------------- --------------------------"
     for i in range(0,len(lines)):
-        print lines[i]
+        printSingleJobsDetails(lines[i][:8])
 
 # make this file usable as a script as well
 if __name__ == "__main__":
